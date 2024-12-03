aafc9b84d159928509e29c30009ad555fc99ff72
ritesh <rieshdhapatepatil@gmail.com>
2024-12-03 12:01:47 +0530

    added filter file

--- scrapers/list_sec_filings.py
+++ scrapers/list_sec_filings.py
@@ -1,21 +1,36 @@
 import requests
 
 def list_sec_filings(symbol: str):
+    """
+    Fetch and filter SEC filings for a given symbol, returning only the latest filing for each form type.
     
-    baseUrl = f"https://api.nasdaq.com/api/company/{symbol.upper()}/sec-filings?sortColumn=filed&sortOrder=desc"
-    print(baseUrl)
+    Args:
+        symbol (str): The stock symbol to fetch filings for.
+        
+    Returns:
+        dict: Dictionary with form types as keys and their latest filing as values.
+    """
+    base_url = f"https://api.nasdaq.com/api/company/{symbol.upper()}/sec-filings?sortColumn=filed&sortOrder=desc"
+    print(f"Fetching data from: {base_url}")
     
-    response = requests.get(baseUrl, timeout=10, headers={"User-Agent": "Mozilla/5.0"})
-    
+    response = requests.get(base_url, timeout=10, headers={"User-Agent": "Mozilla/5.0"})
     body = response.json()
     
     if body["status"]["rCode"] != 200:
-        raise Exception(f"Failed to fetch data for {symbol} - {body["status"]["bCodeMessage"][0]["errorMessage"]}")
+        raise Exception(f"Failed to fetch data for {symbol} - {body['status']['developerMessage']}")
     
     data = body["data"]["rows"]
     
+    # Filter Latest Filings by Form Type
+    filtered_filings = {}
+    for row in data:
+        form_type = row.get("formType")
+        if form_type and form_type not in filtered_filings:
+            filtered_filings[form_type] = row
     
-    # TODO Filter Latest Filings Types
-    
-    return data
-    
+    filterRowList=[]
+    for data in filtered_filings.values():
+        filterRowList.append(data)
+
+        
+    return filterRowList
